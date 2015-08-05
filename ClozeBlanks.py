c3f2f12279b31de20eaa040d489a8fc4911ad21c
Arthaey <arthaey@gmail.com>
2015-08-05 16:19:45 -0500

    Adds blanks to all existing cloze notes.

--- ClozeBlanks.py
+++ ClozeBlanks.py
@@ -1,30 +1,45 @@
 # See github page to report issues or to contribute:
 # https://github.com/Arthaey/anki-cloze-blanks
 
+import re
 from PyQt4.QtCore import SIGNAL
 from PyQt4.QtGui import QAction, QProgressDialog
 
 from anki.hooks import wrap
 from aqt import mw
 from aqt.editor import Editor
-from aqt.utils import askUser
-
-import pprint, sys # DELETE
-pp = pprint.PrettyPrinter(indent = 2, stream=sys.stderr) # DELETE
+from aqt.utils import askUser, showInfo
 
 FEATURES = {
-    "forNewCards" : True,
+    "forNewCards" : False, # TODO: not yet implemented
     "forExistingCards" : True,
 }
 
+# TODO: not yet implemented
 def addClozeBlanksToNewCards(self):
-    pp.pprint(self) # DELETE
+    pass
 
 def addClozeBlanksToExistingCards():
     if not askUser(_(u"Add blanks to all cloze cards?")):
         return
-    nids = mw.col.db.list("select id from notes") # FIXME
-    pp.pprint(nids) # DELETE
+    cloze = mw.col.models.byName("Cloze")
+    nids = mw.col.models.nids(cloze)
+    for nid in nids:
+        note = mw.col.getNote(nid)
+        text = note["Text"]
+        # Only update clozes that do not already have hint text.
+        clozes = re.sub(r"{{c(\d+)::([^:]+?)}}", _addClozeBlanks, text)
+        note["Text"] = clozes
+        note.flush()
+    showInfo(u"Updated {0} notes.".format(len(nids)))
+
+def _addClozeBlanks(match):
+    num = match.group(1)
+    text = match.group(2)
+    words = text.split(" ")
+    blanks = " ".join(["_" * max(1, len(word)/2) for word in words])
+    # Need to escape curly-braces.
+    return u"{{{{c{0}::{1}::{2}}}}}".format(num, text, blanks)
 
 
 if FEATURES["forNewCards"]:
@@ -32,6 +47,7 @@ if FEATURES["forNewCards"]:
 
 if FEATURES["forExistingCards"]:
     add_nid = QAction(mw)
+    mw.form.menuTools.addSeparator()
     mw.form.menuTools.addAction(add_nid)
     add_nid.setText(_(u"Add blanks to cloze notes"))
     mw.connect(add_nid, SIGNAL("triggered()"), addClozeBlanksToExistingCards)
--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-Anki plugin that adds "fill in the blank"-style hints to cloze cards. For
+# Description
+
+An Anki add-on that adds "fill in the blank"-style hints to cloze cards. For
 example, a cloze card with `{{c1:fill in the blanks}}` will become
-`{{c1:fill in the blanks::__ _ _ __}`. It ignores clozes that already have
-custom text set.
+`{{c1:fill in the blanks::__ _ _ ___}`. It ignores clozes that already have
+a custom hint set.
+
+To run, select the "Add blanks to cloze notes" item under the Tools menu.
+
+# Requirements
+
+This add-on assumes you have a "Cloze" note type.
+
+# Support
+
+Post a [new issue on Github](https://github.com/Arthaey/anki-cloze-blanks/issues/new)
+(or make a pull request!).
+
+[My other Anki add-ons](https://github.com/search?q=user%3AArthaey+anki)
+are also on Github.
+
+# TODO
+
+- automatically create cloze blanks when creating new cards
+
+# License
+
+This addon is licensed under the same license as Anki itself (GNU Affero General
+Public License 3).
